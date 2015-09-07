ea46785344f36879b51c83b466b3f8379f53efdb
Matt Arnold <matthew.arnold@m-w.co.uk>
2015-09-07 14:43:48 +0100

    Added the plugin.

---
+++ scsscompile.py
@@ -0,0 +1,15 @@
+import sublime, sublime_plugin
+from subprocess import Popen, PIPE, STDOUT
+
+class scsscompileCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    for region in self.view.sel():  
+            if not region.empty():  
+                # Get the selected text  
+                s = self.view.substr(region)
+                p = Popen(['scss'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+                stdout_data = p.communicate(input=bytes(s, 'UTF-8'))[0]
+                # print(stdout_data)
+                stdout_data = str(stdout_data).replace('\\n', '\n')
+                stdout_data = stdout_data[2:-1]
+                self.view.replace(edit, region, stdout_data)
