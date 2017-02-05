b20fb2674dc35a8f3a0675b40ecdfffb3be130ef
Jamie Scott <JayRScott@gmail.com>
2017-02-05 20:16:58 +0000

    Adding Windows support

--- scsscompile.py
+++ scsscompile.py
@@ -1,15 +1,21 @@
 import sublime, sublime_plugin
 from subprocess import Popen, PIPE, STDOUT
+from sys import platform
+
+if platform == 'win32':
+    scss = 'scss.bat'
+else:
+    scss = '/usr/local/bin/scss'
 
 class scsscompileCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    for region in self.view.sel():  
-            if not region.empty():  
-                # Get the selected text  
+    for region in self.view.sel():
+            if not region.empty():
+                # Get the selected text
                 s = self.view.substr(region)
-                p = Popen(['/usr/local/bin/scss'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+                p = Popen([scss], stdout=PIPE, stdin=PIPE, stderr=PIPE)
                 stdout_data = p.communicate(input=bytes(s, 'UTF-8'))[0]
                 # print(stdout_data)
-                stdout_data = str(stdout_data).replace('\\n', '\n')
-                stdout_data = stdout_data[2:-1]
+                stdout_data = str(stdout_data).replace('\\n', '\n').replace('\\r', '')
+                stdout_data = stdout_data[2:-2]
                 self.view.replace(edit, region, stdout_data)
